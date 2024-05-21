99b761ad35fa79472489cac7530fc2245b5efb5c
Andre Moreira Magalhaes <andre.magalhaes@collabora.com>
2024-05-21 14:17:52 -0300

    PyTorch/llm: Update copyright notices
    Signed-off-by: Andre Moreira Magalhaes <andre.magalhaes@collabora.com>

--- PyTorch/llm/app.py
+++ PyTorch/llm/app.py
@@ -1,3 +1,9 @@
+# Copyright (c) Microsoft Corporation.
+# Copyright (c) Collabora Limited.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 import sys
 from pathlib import Path
 from typing import Union
--- PyTorch/llm/scripts/download_and_convert.py
+++ PyTorch/llm/scripts/download_and_convert.py
@@ -1,8 +1,10 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-
+# Copyright (c) Microsoft Corporation.
+# Copyright (c) Collabora Limited.
+#
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
+
 import os
 import json
 import re
